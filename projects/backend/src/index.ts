e595ddb3808e41ea9124c58975db833900b91877
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-09-19 10:03:31 +0700

    feat: backend services

---
+++ projects/backend/src/index.ts
@@ -0,0 +1,27 @@
+import '@busy-hour/blaze-trpc';
+import { BlazeTrpcRouter } from '@busy-hour/blaze-trpc/types';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Blaze } from '@busy-hour/blaze';
+import { cors } from '@busy-hour/blaze/cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const servicePath = path.resolve(__dirname, 'services');
+
+const app = new Blaze();
+
+await app.load({
+  path: servicePath,
+  middlewares: [['ALL', cors()]],
+  autoStart: true,
+});
+
+app.useTrpc('/trpc/*');
+
+const config = app.serve(3000);
+
+export { BlazeTrpcRouter };
+
+export default config;
